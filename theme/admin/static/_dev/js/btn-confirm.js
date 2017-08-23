d6092c981b5669f4aa9a806156767d9026361fe1
iqbalfn <iqbalfawz@gmail.com>
2017-08-23 12:26:38 +0700

    Set default btn confirm label

--- theme/admin/static/_dev/js/btn-confirm.js
+++ theme/admin/static/_dev/js/btn-confirm.js
@@ -7,7 +7,13 @@
         var title   = $this.data('confirm');
         var message = $this.data('confirm-message');
         var type    = $this.data('confirm-type') || 'danger';
-        var label   = $this.data('confirm-label') || 'Delete';
+        var label   = $this.data('confirm-label');
+        if(!label){
+            if(type == 'danger')
+                label = 'Delete';
+            else
+                label = 'Oke';
+        }
         
         bootbox.confirm({
             title: title,
--- theme/admin/static/js/portal.js
+++ theme/admin/static/js/portal.js
@@ -6422,7 +6422,13 @@ $(function(){
         var title   = $this.data('confirm');
         var message = $this.data('confirm-message');
         var type    = $this.data('confirm-type') || 'danger';
-        var label   = $this.data('confirm-label') || 'Delete';
+        var label   = $this.data('confirm-label');
+        if(!label){
+            if(type == 'danger')
+                label = 'Delete';
+            else
+                label = 'Oke';
+        }
         
         bootbox.confirm({
             title: title,
